544504ad1658bf8bc1b50053a33bbd6ae50d930d
mansibavishi-inventyv <mansi.bavishi@inventyv.com>
2025-10-31 16:50:51 +0530

    Remove parent component

--- dist/inventyv-editor/lib/components/html-editor/inventyv-html-editor/inventyv-html-editor.component.d.ts
+++ dist/inventyv-editor/lib/components/html-editor/inventyv-html-editor/inventyv-html-editor.component.d.ts
@@ -1,7 +1,14 @@
+import { ElementRef } from '@angular/core';
+import { EditorView } from 'prosemirror-view';
 import * as i0 from "@angular/core";
 export declare class InventyvHtmlEditorComponent {
+    editorHost: ElementRef<HTMLDivElement>;
     contentTemplate: string;
+    html: EditorView;
     ngOnInit(): void;
+    loadTemplate(template: string | null): void;
+    setContentFromHTML(html: string | null): void;
+    getHTML(): string;
     static ɵfac: i0.ɵɵFactoryDeclaration<InventyvHtmlEditorComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<InventyvHtmlEditorComponent, "lib-inventyv-html-editor", never, { "contentTemplate": { "alias": "contentTemplate"; "required": false; }; }, {}, never, never, true, never>;
 }
