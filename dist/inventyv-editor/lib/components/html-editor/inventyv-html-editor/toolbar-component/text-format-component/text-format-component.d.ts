aa591758d9e0e28d66a086e118bef2f095220e55
mansibavishi-inventyv <mansi.bavishi@inventyv.com>
2025-10-31 16:19:05 +0530

    Add utils in public.api

--- dist/inventyv-editor/lib/components/html-editor/inventyv-html-editor/toolbar-component/text-format-component/text-format-component.d.ts
+++ dist/inventyv-editor/lib/components/html-editor/inventyv-html-editor/toolbar-component/text-format-component/text-format-component.d.ts
@@ -5,6 +5,7 @@ export declare class TextFormatComponent {
     toggleItalic(): void;
     toggleUnderLine(): void;
     toggleStrike(): void;
+    ngOnInit(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<TextFormatComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<TextFormatComponent, "app-text-format-component", never, { "content": { "alias": "content"; "required": false; }; }, {}, never, never, true, never>;
 }
--- dist/inventyv-editor/public-api.d.ts
+++ dist/inventyv-editor/public-api.d.ts
@@ -1,3 +1,6 @@
 export * from './lib/components/html-editor/inventyv-html-editor/inventyv-html-editor.component';
 export * from './lib/components/html-editor/inventyv-html-editor/html-editor/html-editor/html-editor.component';
 export * from './lib/components/html-editor/inventyv-html-editor/toolbar-component/toolbar-component';
+export * from './lib/components/html-editor/inventyv-html-editor/utils/editor-schema';
+export * from './lib/components/html-editor/inventyv-html-editor/utils/editor-plugins';
+export * from './lib/components/html-editor/inventyv-html-editor/utils/editor-keymaps';
